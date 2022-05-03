5e81c70964d817b787c4c99c531e4645419d3dcf
Jake Mulford <jakemulf@gmail.com>
2022-05-03 12:46:23 -0400

    added support for promoted tweets and refactored to a semi class-based model (#11)

--- app.d/twitter_main.py
+++ app.d/twitter_main.py
@@ -8,7 +8,7 @@ to be called in the Deephaven UI.
 """
 from deephaven import DynamicTableWriter
 import deephaven.dtypes as dht
-from deephaven.time import plus_period, to_datetime
+from deephaven.time import plus_period, to_datetime, to_period
 
 from twitter_ads.client import Client
 from twitter_ads.analytics import Analytics
@@ -24,7 +24,51 @@ TWITTER_CONSUMER_SECRET = os.environ.get("TWITTER_CONSUMER_SECRET")
 TWITTER_ACCESS_TOKEN = os.environ.get("TWITTER_ACCESS_TOKEN")
 TWITTER_ACCESS_TOKEN_SECRET = os.environ.get("TWITTER_ACCESS_TOKEN_SECRET")
 
+PROMOTED_TWEET_DURATION = to_period("14D")
+
 twitter_client = Client(TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET, TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_TOKEN_SECRET)
+
+class TwitterCollector:
+    """
+    A class to nicely define what data to collect from Twitter's ads API
+
+    Attributes:
+        analytics_items (list<tuple>): A list of tuples that contains the following:
+            API analytics name, Deephaven table column name, twitter account, twitter analytics,
+            and the analytics range method
+    """
+
+    def __init__(self, twitter_client, analytics_types):
+        """
+        Constructor method
+
+        Parameters:
+            twitter_client (Client): The Twitter client to make API calls
+            analytics_types (list<tuple>): A list of tuples containing the following:
+            API analytics name, Deephaven table column name, the twitter analytics method to pull from,
+            and the analytics range method. This is used to build the analytics_items attribute
+        """
+        self.analytics_items = []
+        for account in twitter_client.accounts():
+            for (api_name, table_name, analytics_list_method, out_of_range) in analytics_types:
+                for analytics in analytics_list_method(account):
+                    self.analytics_items.append((api_name, table_name, account, analytics, out_of_range))
+
+def promoted_tweet_out_of_range(promoted_tweet, start_date, end_date):
+    """
+    Determines if the promoted tweet exists within the given date range.
+
+    Parameters:
+        promoted_tweet (PromotedTweet): The Twitter promoted tweet object
+        start_date (DateTime): The start date as a Deephaven DateTime object
+        end_date (DateTime): The end date as a Deephaven DateTime object
+    Returns:
+        bool: True if the promoted tweet is out of the given date range, False otherwise
+    """
+    promoted_tweet_start_time = to_datetime(promoted_tweet._created_at[0:-1] + " UTC")
+    promoted_tweet_end_time = plus_period(promoted_tweet_start_time, PROMOTED_TWEET_DURATION)
+
+    return (promoted_tweet_start_time >= start_date and promoted_tweet_start_time >= end_date) or (promoted_tweet_end_time <= end_date and promoted_tweet_end_time <= start_date)
 
 def analytics_out_of_range(analytics, start_date, end_date):
     """
@@ -69,9 +113,6 @@ def get_analytics_metrics(account, analytics, start_date, end_date, placement, e
     Returns:
         str: A JSON string of the analyitcs response
     """
-    if analytics_out_of_range(analytics, start_date, end_date):
-        return None
-
     metric_groups = [METRIC_GROUP.ENGAGEMENT]
     kwargs = {
         "start_time": datetime.strptime(start_date.toDateString(), "%Y-%m-%d"),
@@ -93,7 +134,7 @@ def get_campaigns(account):
         account (Account): The Twitter account to pull data from
 
     Returns:
-        list<Campaign>: The list of all campaigns across all accounts 
+        list<Campaign>: The list of all campaigns across the account
     """
     campaigns = []
     for campaign in account.campaigns():
@@ -109,7 +150,7 @@ def get_line_items(account):
         account (Account): The Twitter account to pull data from
 
     Returns:
-        list<LineItem>: The list of all line items across all accounts 
+        list<LineItem>: The list of all line items across the account
     """
     line_items = []
     for line_item in account.line_items():
@@ -118,6 +159,15 @@ def get_line_items(account):
     return line_items
 
 def get_funding_instruments(account):
+    """
+    Retrieves all the funding instruments for the Twitter account
+
+    Parameters:
+        account (Account): The Twitter account to pull data from
+
+    Returns:
+        list<FundingInstrument>: The list of all funding instruments across the account
+    """
     funding_instruments = []
     for funding_instrument in account.funding_instruments():
         funding_instruments.append(funding_instrument)
@@ -125,6 +175,15 @@ def get_funding_instruments(account):
     return funding_instruments
 
 def get_promoted_tweets(account):
+    """
+    Retrieves all the promoted tweets for the Twitter account
+
+    Parameters:
+        account (Account): The Twitter account to pull data from
+
+    Returns:
+        list<PromotedTweet>: The list of all promoted tweets across the account
+    """
     promoted_tweets = []
     for promoted_tweet in account.promoted_tweets():
         promoted_tweets.append(promoted_tweet)
@@ -132,6 +191,15 @@ def get_promoted_tweets(account):
     return promoted_tweets
 
 def get_media_creatives(account):
+    """
+    Retrieves all the media creatives for the Twitter account
+
+    Parameters:
+        account (Account): The Twitter account to pull data from
+
+    Returns:
+        list<MediaCreative>: The list of all media creatives across the account
+    """
     media_creatives = []
     for media_creative in account.media_creatives():
         media_creatives.append(media_creative)
@@ -162,18 +230,20 @@ def twitter_ads_main(start_date, end_date, date_increment):
 
     #Get metrics on the account. These tuples represent the
     #analytics name for the Twitter API, the name of the row
-    #in the AnalyticsType column, and the function that takes an account
-    #and returns a list of entities to pull.
+    #in the AnalyticsType column, the function that takes an account
+    #and returns a list of entities to pull, and the function
+    #that tells whether the metric is in the date range or not
 
     #To collect more data, add entries to this list
     analytics_types = [
-        ("CAMPAIGN", "Campaign", get_campaigns),
-        ("LINE_ITEM", "AdGroup", get_line_items),
-        ("FUNDING_INSTRUMENT", "FundingInstrument", get_funding_instruments),
-        #("PROMOTED_TWEET", "PromotedTweet", get_promoted_tweets[0:1]), #Leaving this out for now
-        #since the promoted tweets don't have a start/end time, but instead a created time
-        ("MEDIA_CREATIVE", "MediaCreative", get_media_creatives)
+        ("CAMPAIGN", "Campaign", get_campaigns, analytics_out_of_range),
+        ("LINE_ITEM", "AdGroup", get_line_items, analytics_out_of_range),
+        ("FUNDING_INSTRUMENT", "FundingInstrument", get_funding_instruments, analytics_out_of_range),
+        ("PROMOTED_TWEET", "PromotedTweet", get_promoted_tweets, promoted_tweet_out_of_range), 
+        ("MEDIA_CREATIVE", "MediaCreative", get_media_creatives, analytics_out_of_range)
     ]
+
+    twitter_collector = TwitterCollector(twitter_client, analytics_types)
 
     #Loop through dates
     current_date = start_date
@@ -182,13 +252,12 @@ def twitter_ads_main(start_date, end_date, date_increment):
         print(current_date)
         next_date = plus_period(current_date, date_increment)
 
-        for account in twitter_client.accounts():
-            for (api_name, table_name, analytics_list_method) in analytics_types:
-                for analytics in analytics_list_method(account):
-                    for placement in ["PUBLISHER_NETWORK", "ALL_ON_TWITTER"]:
-                        json_str = get_analytics_metrics(account, analytics, current_date, next_date, placement, api_name)
-                        if not (json_str is None):
-                            table_writer.write_row(current_date, account.name, table_name, placement, json_str)
+        for (api_name, table_name, account, analytics, out_of_range) in twitter_collector.analytics_items:
+            if not out_of_range(analytics, current_date, next_date):
+                for placement in ["PUBLISHER_NETWORK", "ALL_ON_TWITTER"]:
+                    json_str = get_analytics_metrics(account, analytics, current_date, next_date, placement, api_name)
+                    table_writer.write_row(current_date, account.name, table_name, placement, json_str)
+
         current_date = next_date
 
     return table_writer.table
